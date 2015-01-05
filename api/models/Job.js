62879d0f11e3ccf763a5c74f816856b84ba25361
Nathan Pointer <nwpointer@gmail.com>
2015-01-05 02:11:27 -0800

    basic model stubs

---
+++ api/models/Job.js
@@ -0,0 +1,19 @@
+/**
+* Job.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  schema:true,
+
+  attributes: {
+
+    title : { type: 'string', required: true },
+
+    description : { type: 'string', required: true }
+  }
+};
+
---
+++ api/models/JobListing.js
@@ -0,0 +1,19 @@
+/**
+* JobListing.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  schema: true	
+
+  attributes: {
+
+    published : { type: 'date' },
+
+    expires : { type: 'date' }
+  }
+};
+
